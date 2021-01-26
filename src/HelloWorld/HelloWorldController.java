8e08dc65d70ab3d3da110e91872835af0731be3c
NicholasGin <nicholasgin@gmail.com>
2021-01-26 02:30:46 -0500

    renamed files
    updated fxml file
    - displays clickable button to display hello world

---
+++ src/HelloWorld/HelloWorldController.java
@@ -0,0 +1,22 @@
+package HelloWorld;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class HelloWorldController implements Initializable {
+    @FXML
+    private Label label;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        label.setText("Hello World!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+}
